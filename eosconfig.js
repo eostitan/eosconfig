180d09fe6a2eda9f3f8b27175f2490f4467b786e
systemzax <gbabint@msn.com>
2018-05-27 09:38:38 -0400

    Misc

--- eosconfig.js
+++ eosconfig.js
@@ -211,7 +211,7 @@ function main(){
 	}
 
  	function launchNodeos(cb){
-		nodeos  = exec('nodeos -e -p eosio --genesis-json /home/g/.local/share/eosio/nodeos/config/genesis.json --config-dir /home/g/.local/share/eosio/nodeos/config --plugin eosio::chain_api_plugin --plugin eosio::producer_plugin --plugin eosio::history_api_plugin --plugin eosio::history_plugin --plugin eosio::http_plugin');
+		nodeos  = exec('nodeos --genesis-json /home/g/.local/share/eosio/nodeos/config/genesis.json');
 
 		setTimeout(function(){
 
